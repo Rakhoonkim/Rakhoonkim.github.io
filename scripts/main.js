7fa0545a0f6f661f626cd0490ee63ca9d6189569
gamekrh1130 <gamekrh1130@neowiz.com>
2024-09-08 12:47:16 +0900

    수정 - fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 // Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-$(document).ready(function () {
-    $("#map-image").on("click")
-    {
 
-    }
+
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(
+
+function () {
+    var ddd = "ssss";
 
     $('#go-to-top').click(function () {
         $('html,body').animate({scrollTop: 0}, 400);
